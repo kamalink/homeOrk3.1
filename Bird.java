f6536e07fcdad062e507c7f44521fbbee7f43b9d
kamalink <hibritman@ukr.net>
2017-01-05 12:28:54 +0200

    ok

---
+++ Bird.java
@@ -0,0 +1,7 @@
+package module3.homeOrk1;
+
+class Bird {
+     void sing(){
+        System.out.println("I am walking \n" + "I am flying \n" + "I am singing \n" + "I am Bird");
+    }
+}
---
+++ BirdMain.java
@@ -0,0 +1,9 @@
+package module3.homeOrk1;
+
+class BirdMain {
+    public static void main(String[] args) {
+        Bird simplyBird = new Bird();
+
+        simplyBird.sing();
+    }
+}
